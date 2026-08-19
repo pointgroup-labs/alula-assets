05045dd4cd21b2f7109baf456828168023d0a7dd
tiamo <vk.tiamo@gmail.com>
2026-08-19 04:12:18 +0300

    feat(tokens): add xSolvBTC to the registry

---
+++ src/tokens/xsolvbtc.ts
@@ -0,0 +1,13 @@
+import tokenIcon from '../img/xsolvbtc.webp';
+import { RegistryAsset } from '../types';
+
+export const asset: RegistryAsset = {
+    id: "xsolvbtc",
+    name: "xSolvBTC",
+    symbol: "xSolvBTC",
+    icon: tokenIcon,
+    decimals: 8,
+    token_addresses: {
+        mainnet: "CAUP7NFABXE5TJRL3FKTPMWRLC7IAXYDCTHQRFSCLR5TMGKHOOQO772J",
+    }
+}
